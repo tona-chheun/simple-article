42388b48c9e9db3717138bfe9ca948f87d8c2ee1
Tona <tona-chheun@users.noreply.github.com>
2026-06-21 13:26:34 +0700

    refactor prefix endpoints

--- src/main/java/com/example/simplearticle/controllers/ArticleController.java
+++ src/main/java/com/example/simplearticle/controllers/ArticleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/articles")
+@RequestMapping("articles")
 public class ArticleController {
     private final ArticleService articleService;
 
